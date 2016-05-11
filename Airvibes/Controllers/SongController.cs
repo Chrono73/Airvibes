10b453ddb57494b07b8723f9dbe7d448764af74a
Martin <martin.locqueville@wanadoo.fr>
2016-05-11 15:07:22 +0200

    Premier commit, seule la BDD est initialisée, sans les fichiers audio.

---
+++ Airvibes/Controllers/SongController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Airvibes.Models;
+
+namespace Airvibes.Controllers
+{
+    public class SongController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Song
+        public ActionResult Index()
+        {
+            return View(db.Songs.ToList());
+        }
+
+        // GET: Song/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Songs songs = db.Songs.Find(id);
+            if (songs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(songs);
+        }
+
+        // GET: Song/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Song/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Id_Artist,Id_Record,Title,Duration,TimesPlayed,Rating,LowQFile,HiQFile")] Songs songs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Songs.Add(songs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(songs);
+        }
+
+        // GET: Song/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Songs songs = db.Songs.Find(id);
+            if (songs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(songs);
+        }
+
+        // POST: Song/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Id_Artist,Id_Record,Title,Duration,TimesPlayed,Rating,LowQFile,HiQFile")] Songs songs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(songs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(songs);
+        }
+
+        // GET: Song/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Songs songs = db.Songs.Find(id);
+            if (songs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(songs);
+        }
+
+        // POST: Song/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Songs songs = db.Songs.Find(id);
+            db.Songs.Remove(songs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
